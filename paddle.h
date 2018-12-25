6c99e735c83ba5899f627ac8a3804844af06dce1
ostrenko-ta <46007615+TaisiaO@users.noreply.github.com>
2018-12-25 14:58:38 +0300

    Update paddle.h

--- paddle.h
+++ paddle.h
@@ -12,8 +12,6 @@ class Paddle {
   public:
     Paddle();
     ~Paddle();
-
-  public:
     void resetState();
     void move();
     void setDx(int);
@@ -23,9 +21,9 @@ class Paddle {
   private:
     QImage image;
     QRect rect;
-    int dx;
-    static const int INITIAL_X = 173;
-    static const int INITIAL_Y = 460;
+    int dx; // сдвиг
+    static const int INITIAL_X = 173;//координата начальной позиции по оси х
+    static const int INITIAL_Y = 460;//координата начальной позиции по оси у
 
 };
 
