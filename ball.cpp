4bbb6eaa9fe6c1f8fa46c2f7ae213ba07df15c14
ostrenko-ta <46007615+TaisiaO@users.noreply.github.com>
2018-12-25 14:15:07 +0300

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -3,13 +3,11 @@
 
 Ball::Ball() {
 
-  xdir = 1;
-  ydir = -1;
-
+  xdir = 1; //направление полета шарика по оси x
+  ydir = -1; //направление полета шарика по оси y
   image.load(":/images/Images/ball2.png");
-
-  rect = image.rect();
-  resetState();
+  rect = image.rect(); // прямоугольник, соответствующий по форме и размеру полученному изображению
+  resetState(); // помещаем шарик в начальное положение 
 }
 
 Ball::~Ball() {
@@ -18,53 +16,43 @@ Ball::~Ball() {
 }
 
 void Ball::autoMove() {
-
-  rect.translate(xdir, ydir);
+ rect.translate(xdir, ydir); // получаем сдвинутую dx и dy на копию прямоугольника
 
   if (rect.left() == 0) {
     xdir = 1;
   }
-
   if (rect.right() == RIGHT_EDGE) {
     xdir = -1;
   }
-
   if (rect.top() == 0) {
     ydir = 1;
   }
 }
 
 void Ball::resetState() {
-
   rect.moveTo(INITIAL_X, INITIAL_Y);
 }
 
 void Ball::setXDir(int x) {
-
   xdir = x;
 }
 
 void Ball::setYDir(int y) {
-
   ydir = y;
 }
 
 int Ball::getXDir() {
-
   return xdir;
 }
 
 int Ball::getYDir() {
-
   return ydir;
 }
 
 QRect Ball::getRect() {
-
   return rect;
 }
 
 QImage & Ball::getImage() {
-
   return image;
 }
