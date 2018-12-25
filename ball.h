7d7e07eafc02820d68a09b2e7de16462d358270a
ostrenko-ta <46007615+TaisiaO@users.noreply.github.com>
2018-12-25 14:26:09 +0300

    Update ball.h

--- ball.h
+++ ball.h
@@ -12,8 +12,6 @@ class Ball {
   public:
     Ball();
     ~Ball();
-
-  public:
     void resetState();
     void autoMove();
     void setXDir(int);
@@ -28,9 +26,9 @@ class Ball {
     int ydir;
     QImage image;
     QRect rect;
-    static const int INITIAL_X = 187;
-    static const int INITIAL_Y = 449;
-    static const int RIGHT_EDGE = 380;
+    static const int INITIAL_X = 187;//координаты начальной позиции по х
+    static const int INITIAL_Y = 449;//координаты начальной позиции по у
+    static const int RIGHT_EDGE = 380;//правый край игрового поля
 };
 
 #endif // BALL_H
